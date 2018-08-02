6bfe716ebbdb873df68b7427adef44c634a4bee5
Viktor Grom <viktor@placesscout.com>
2018-08-02 00:36:59 -0700

    fix mistakes

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,27 @@
+$(document)
+  .ready(() => {
+    $('form')
+      .on('click', (e) => {
+        e.preventDefault();
+        let flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+        let photo = $("#search")
+          .val();
+        let flickrOptions = {
+          tags: photo,
+          format: "json"
+        };
 
-
-$(document).ready( ()=>{
-  $('form').on('click', ()=>{
-    // $(this).removeClass("selected");
-    // $(this).addClass("selected");
-
-    var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-
-    var animal = $("#search").val();
-
-    var flickrOptions = {
-      tags: animal,
-      format: "json"
-    };
-
-    function displayPhotos(data) {
-      var photoHTML = '<ul>';
-      $.each(data.items, function(i, photo) {
-      photoHTML += '<li class="grid-25 tablet-grid-50">';
-      photoHTML +=  '<a href="' + photo.link + '" class="image">';
-      photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+        function displayPhotos(data) {
+          var photoHTML = '<ul>';
+          $.each(data.items, function(i, photo) {
+            photoHTML += '<li class="grid-25 tablet-grid-50">';
+            photoHTML += '<a href="' + photo.link + '" class="image">';
+            photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+          });
+          photoHTML += '</ul>';
+          $("#photos")
+            .html(photoHTML);
+        }
+        $.getJSON(flickerAPI, flickrOptions, displayPhotos);
       });
-      photoHTML += '</ul>';
-      $("#photos").html(photoHTML);
-    }
-
-    $.getJSON(flickerAPI, flickrOptions, displayPhotos);
   });
-});
